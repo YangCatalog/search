f806f49db3e32b260f58b552ba879519e0fd0ac2
miroslav.kovac <miroslav.kovac@pantheon.tech>
2020-08-26 11:50:21 +0200

    Update alert message
    Signed-off-by: miroslav.kovac <miroslav.kovac@pantheon.tech>

--- search/views.py
+++ search/views.py
@@ -851,7 +851,7 @@ def search(post_json, search_term, alerts):
         response = requests.post('{}/api/fast'.format(api_prefix), json=post_json,
                                  headers={'Content-type': 'application/json', 'Accept': 'application/json'})
         if response.status_code == 400:
-            alerts.append(response.json().get('error'))
+            alerts.append(response.json().get('description'))
             return ''
         results = response.json().get('results')
 
